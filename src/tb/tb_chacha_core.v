46e0c3864a8e40e2c34ea957009003cea8f27dde
Joachim Strömbergson <joachim@secworks.se>
2013-09-12 16:02:04 +0200

    Fixed the format.

--- src/tb/tb_chacha_core.v
+++ src/tb/tb_chacha_core.v
@@ -175,7 +175,7 @@ module tb_chacha_core();
       $display("x12_reg = %08x, x13_reg = %08x", dut.x12_reg, dut.x13_reg);
       $display("x14_reg = %08x, x15_reg = %08x", dut.x14_reg, dut.x15_reg);
       $display("");
-      $display("qr_ctr_reg  = %x, dr_ctr_reg  = %x", dut.qr_ctr_reg, dut.dr_ctr_reg);
+      $display("qr_ctr_reg = %01x, dr_ctr_reg  = %01x", dut.qr_ctr_reg, dut.dr_ctr_reg);
       $display("block0_ctr_reg = %08x, block1_ctr_reg = %08x", dut.block0_ctr_reg, dut.block1_ctr_reg);
       $display("");
     end
